09f4f18c67f2109da89bd28027c61584e279e72c
Chris Honeycutt <clhoneycutt83@gmail.com>
2020-03-14 20:49:41 -0400

    First commit. Basic structure created.

---
+++ src/components/dashboard/dashboard.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './dashboard.styles.scss';
+
+const Dashboard = () => (
+  <div className="dashboard">
+    
+  </div>
+)
+
+export default Dashboard;
---
+++ src/pages/homepage/homepage.page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "tachyons";
+
+
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+  <div className="homepage tc">
+    <h1>HomePage</h1>
+  </div>
+);
+
+export default HomePage;
